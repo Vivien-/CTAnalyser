cae2fe7a88980e9d9680ec3d43fedb5713e1adb7
Vivien <vachet@enseirb.fr>
2016-03-09 17:15:24 +0100

    deleting useless file

--- src/patch.hpp
+++
@@ -1,9 +0,0 @@
-namespace patch
-{
-    template < typename T > std::string to_string( const T& n )
-    {
-        std::ostringstream stm ;
-        stm << n ;
-        return stm.str() ;
-    }
-}
